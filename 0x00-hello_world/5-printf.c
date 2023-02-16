8b1308ade18f23b099445f040f30d0913d40fd45
Yegetaneh <lijnati3@gmail.com>
2023-02-16 05:48:12 -0800

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,7 @@
  * Return: 0 for successful exit
  */
 
-int main(void) 
-{
-   printf("with proper grammar, but the outcome is a piece of art,\n");
-   return (0);
+int main(void) {
+ printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
 }
