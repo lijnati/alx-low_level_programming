60a308b7b90cb0e8400d31d35e8a732abf64f607
Yegetaneh <lijnati3@gmail.com>
2023-02-15 22:50:35 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,5 +2,17 @@
 
 int main(void) {
  puts("\"Programming is like building a multilingual puzzle");
- return (0);
+#include <stdio.h>
+
+/**
+ *main : Entry Point
+ *
+ *Return: 0 for succesful exit
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+} return (0);
 }
