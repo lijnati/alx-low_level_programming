ffb824c7650a0578607211ce128f8036cbf12a85
Yegetaneh <lijnati3@gmail.com>
2023-02-21 09:49:02 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+i#include "main.h"
+
+/**
+   *_abs - returns the absolute value of a number
+   *@n: character to be checked
+   *
+   *Return: This function returns a postive int number
+   */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
