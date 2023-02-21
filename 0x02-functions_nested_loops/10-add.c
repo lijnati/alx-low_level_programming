1eee1e7fad45718991c5ba5db3fdd71780427904
Yegetaneh <lijnati3@gmail.com>
2023-02-21 10:23:59 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - function that adds the two input integers
+ *@i: first integer
+ *@j: second integer
+ *
+ * Return: sum of the two integers.
+ */
+
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+
+}
