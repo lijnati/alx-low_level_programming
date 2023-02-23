d2a6e491618821e0818fe6ea64e9d2b86f64b95d
Yegetaneh <lijnati3@gmail.com>
2023-02-23 01:20:34 -0800

    When debugging, novices insert corrective code; experts remove defective code

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+   *main - This function tests the postive or negative function
+   *Return: This function returns 0 on success
+   */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+# 0x03 Debugging Project
+contains 4 tasks
