0523a46ff8144f09b4bad8bdd4783dc54d9f971d
ceoyegetaneh <lijnati3@gmail.com>
2023-03-10 22:21:12 +0400

    static lib

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
+
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,5 +22,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
 
+
+#endif
